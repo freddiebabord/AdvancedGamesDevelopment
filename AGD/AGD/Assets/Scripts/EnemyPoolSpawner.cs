a8b3d12a36a14a6da42b981bbddbe0b7e2d76a41
Frederic Babord <dmfreddie@gmail.com>
2016-10-08 15:14:10 +0100

    Initial Commit of the project

---
+++ AGD/AGD/Assets/Scripts/EnemyPoolSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public class EnemyPoolSpawner : NetworkBehaviour {
+
+	public GameObject enemyPrefab;
+	
+	private List<GameObject> spawnedEnemies = new List<GameObject>();
+
+	public int SpawnedEnemies { get{ spawnedEnemies.TrimExcess(); return spawnedEnemies.Count; } }
+
+	void Start()
+	{
+		if(!isServer)
+			return;
+			
+		Spawn(5);
+	}
+
+	public void Spawn(int amount)
+	{
+		for(int i = 0; i < amount; ++i)
+		{
+			GameObject newEnemy = (GameObject)Instantiate(enemyPrefab);
+			spawnedEnemies.Add(newEnemy);
+			NetworkServer.Spawn(newEnemy);
+		}
+	}
+}
