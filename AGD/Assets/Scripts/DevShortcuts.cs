9dfe042f249bd330eb5bf1702eb2a66d0728767f
GROVES <N3109532@live.tees.ac.uk>
2016-10-12 16:58:00 +0100

    -First Commit NOTE: *This commit is for testing purposes ONLY!

---
+++ AGD/Assets/Scripts/DevShortcuts.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class DevShortcuts : MonoBehaviour {
+
+    [SerializeField]List<GhostBehaviour> ghosts = new List<GhostBehaviour>();
+
+	// Use this for initialization
+	void Start ()
+    {
+        ghosts = FindObjectsOfType<GhostBehaviour>().ToList();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.F1))
+        {
+            ghosts = FindObjectsOfType<GhostBehaviour>().ToList();
+            print("Hello");
+            foreach (GhostBehaviour ghost in ghosts)
+            {
+                ghost.TakeDamage(GetComponent<NetworkIdentity>().connectionToClient.connectionId);
+            }
+        }
+	}
+}
