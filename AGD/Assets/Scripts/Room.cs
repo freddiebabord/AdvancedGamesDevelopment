39c2d37fbfc01374bf5a3210db908ec3323b22a1
Frederic Babord <dmfreddie@gmail.com>
2016-10-12 11:46:14 +0100

    + Added rooms - can be any shape due to combination of child trigger colliders

---
+++ AGD/Assets/Scripts/Room.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Room : MonoBehaviour {
+
+	public List<Collider> colliders = new List<Collider>();
+	public List<EnemyBase> enemies = new List<EnemyBase>();
+	public List<NetworkedFirstPersonController> players = new List<NetworkedFirstPersonController>();
+	public int enemyCount { get{ return enemies.Count; } }
+	public int playerCount { get{ return players.Count; } }
+
+	// Use this for initialization
+	void Start () {
+		colliders = GetComponentsInChildren<Collider>().ToList();
+	}	
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.GetComponent<EnemyBase>())
+		{
+			enemies.Add(other.GetComponent<EnemyBase>());
+		}
+		else if(other.GetComponent<NetworkedFirstPersonController>())
+		{
+			players.Add(other.GetComponent<NetworkedFirstPersonController>());
+		}
+	}
+
+	public void OnTriggerExit(Collider other)
+	{
+		if(other.GetComponent<EnemyBase>())
+		{
+			enemies.Remove(other.GetComponent<EnemyBase>());
+			enemies.TrimExcess();
+		}
+		else if(other.GetComponent<NetworkedFirstPersonController>())
+		{
+			players.Remove(other.GetComponent<NetworkedFirstPersonController>());
+		}
+	}
+}
---
+++ AGD/Assets/Scripts/RoomCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomCollider : MonoBehaviour {
+
+	private Room room;
+	public Room GetRoom { get { return room; } }
+
+	
+	// Use this for initialization
+	void Start () {
+		room = GetComponentInParent<Room>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		room.OnTriggerEnter(other);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		room.OnTriggerExit(other);
+	}
+
+}
