c365b9762a7d8ae72fe01454bd3a3c54f84c9a06
Frederic Babord <dmfreddie@gmail.com>
2016-11-11 13:48:28 +0000

    + Adding post pro settings

--- AGD/Assets/Scripts/SettingsManager.cs
+++ AGD/Assets/Scripts/SettingsManager.cs
@@ -1,6 +1,21 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.PostProcessing;
+
+public enum AntiAlisatingMode
+{
+    Off,
+    FXAA,
+    TXAA
+}
+
+public enum ReflectionQuality
+{
+    Off,
+    Low,
+    High
+}
 
 public class SettingsManager : MonoBehaviour
 {
@@ -17,6 +32,11 @@ public class SettingsManager : MonoBehaviour
 
     public SplitscreenManager.SplitscreenMode splitscreenMode = SplitscreenManager.SplitscreenMode.Horizontal;
     public float overallVolumeLevel = 100;
+    public bool splitscreenDuelControllerMode = true;
+    public float bloomIntensity = 1;
+    public AntiAlisatingMode aaMode = AntiAlisatingMode.TXAA;
+    public ReflectionQuality reflectionQuality;
+    public PostProcessingProfile profile;
 
     public void SetSplitscreenMode(Int32 value)
     {
@@ -29,5 +49,32 @@ public class SettingsManager : MonoBehaviour
     public void SetMasterVolume(Single volume)
     {
         overallVolumeLevel = volume;
+    }
+
+    public void SetAAMode(int value)
+    {
+        if(value == 0)
+        {
+            aaMode = AntiAlisatingMode.Off;
+            if(profile)
+                profile.antialiasing.enabled = false;
+        }
+        if(value == 1)
+        {
+            aaMode = AntiAlisatingMode.FXAA;
+            if(profile)
+            {
+                profile.antialiasing.enabled = true;
+                profile.antialiasing.settings.method = AntialiasingModel.Method.Fxaa;
+            }
+        }
+        if(value == 2)
+        {
+            aaMode = AntiAlisatingMode.TXAA;
+            if(profile)
+            {
+                profile.antialiasing.enabled = true;
+            }
+        }
     }
 }
