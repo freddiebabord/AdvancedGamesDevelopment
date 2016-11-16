9d9b3acb1807fc9e5fafa77f73142dd81e9bf6a9
LAMB <P4041543@live.tees.ac.uk>
2016-11-16 12:26:55 +0000

    Changes to make radar semi-work. UI needs to be fixed.

--- AGD/Assets/Scripts/Minimap/Radar.cs
+++ AGD/Assets/Scripts/Minimap/Radar.cs
@@ -16,7 +16,7 @@ public class Radar : NetworkBehaviour {
 	public GameObject radar;
 
 	//Play with this map_scale to bring the objects either closer together or further away in the radar.
-	public float map_scale = 5.0f;
+	public float map_scale = 0.1f;
 
 	public List<RadarObject> radar_objects = new List<RadarObject>();
 
@@ -64,11 +64,14 @@ public class Radar : NetworkBehaviour {
 			Vector3 radar_position = rad_obj.owner.transform.position - gameObject.transform.position;
 			float distance_object = Vector3.Distance (gameObject.transform.position, rad_obj.owner.transform.position) * map_scale;
             float delta_y = Mathf.Atan2(radar_position.x, radar_position.z) * Mathf.Rad2Deg - 270 - gameObject.transform.eulerAngles.y;
-			radar_position.x = distance_object * Mathf.Cos(delta_y * Mathf.Deg2Rad) * -1 + (radar.GetComponent<RectTransform>().rect.width/2);
-			radar_position.z = distance_object * Mathf.Sin(delta_y * Mathf.Deg2Rad) + (radar.GetComponent<RectTransform>().rect.height/2);
+            radar_position.x = distance_object * Mathf.Cos(delta_y * Mathf.Deg2Rad) * -1;
+            radar_position.z = distance_object * Mathf.Sin(delta_y * Mathf.Deg2Rad);
 
-			rad_obj.icon.transform.SetParent(radar.gameObject.transform);
-			rad_obj.icon.transform.position = new Vector3(radar_position.x, radar_position.z, 0) + radar.gameObject.transform.position;
+            Transform trans = GameObject.FindGameObjectWithTag("Parent").transform;
+
+			rad_obj.icon.transform.SetParent(trans);
+          //  rad_obj.icon.transform.position = new Vector3(radar_position.x / radar.GetComponent<RectTransform>().rect.width, radar_position.z / radar.GetComponent<RectTransform>().rect.height, 0) + trans.position;
+         //   rad_obj.icon.transform.rotation = new Quaternion(0, 0, 0, 0);
         }
     }
 	
