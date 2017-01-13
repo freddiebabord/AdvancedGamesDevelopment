7efbf9ec5dd24ad0aca81fad7c62d8e999243702
Frederic Babord <dmfreddie@gmail.com>
2017-01-13 02:18:39 +0000

    reverse?

--- AGD/Assets/Scripts/Ghosts/Abilities/GhostReverse.cs
+++ AGD/Assets/Scripts/Ghosts/Abilities/GhostReverse.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class GhostReverse : MonoBehaviour {
 
+	public float screamRadius = 5f;
+	public float screamTime = 2f;
+	public float cooldownTimer = 2f;
+
+	float startCooldownTimer;
+	float startScream;
+
 	// Use this for initialization
 	void Start () {
 	
